ddc1268c59ca092a3ec106cec3df5c9321e70cc4
BohdanGunko <bodya0000@gmail.com>
2020-11-17 14:56:13 +0200

    separate file for some secondary functions in MainWindow class

--- Lab2/Lab2_proj/ClientPart/mainwindow.h
+++ Lab2/Lab2_proj/ClientPart/mainwindow.h
@@ -1,7 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-#include <purchasesmenu.h>
 
+#include <purchasesmenu.h>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -16,16 +16,21 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-
 private slots:
+    void on_ExitButton_clicked();
     void on_RegButton_clicked();
     void on_RegGoBackButton_clicked();
     void on_LogButton_clicked();
-    void resizeEvent(QResizeEvent*);
-    void resizeLogMenu();
-    void resizeRegMenu();
+    void resizeEvent(QResizeEvent*);    //redefinition of QResize event
+    void resizeLogMenu();   //resizing elements in Log menu when window size changes
+    void resizeRegMenu();   //resizing elements in Reg menu when window size changes
+
+
+
 private:
     Ui::MainWindow *ui;
-    PurchasesMenu OvScreen;
+    PurchasesMenu OvScreen; //main menu screen
+    void setShadowEff();    //set shadows in Log and Reg menus
+
 };
 #endif // MAINWINDOW_H
--- Lab2/Lab2_proj/build-ClientPart-Desktop_Qt_5_15_1_MinGW_32_bit-Debug/ui_mainwindow.h
+++ Lab2/Lab2_proj/build-ClientPart-Desktop_Qt_5_15_1_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -389,7 +389,7 @@ public:
 
         retranslateUi(MainWindow);
 
-        stackedWidget->setCurrentIndex(1);
+        stackedWidget->setCurrentIndex(0);
 
 
         QMetaObject::connectSlotsByName(MainWindow);
